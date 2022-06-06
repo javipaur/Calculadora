30671cec4a4f3029cf7a24cd5d9dddc34fa06414
Javipaur <javipaur@gmail.com>
2022-06-06 16:43:40 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import { CalculadoraScreen } from './src/screens/CalculadoraScreen'
+
+ const App = () => {
+  return (
+    <SafeAreaView>
+      <CalculadoraScreen/>
+    </SafeAreaView>
+  )
+}
+
+export default App;
+
